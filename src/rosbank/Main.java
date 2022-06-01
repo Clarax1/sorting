f951fe3f37b7cc90205f8e7810d718d234e4b8e8
Your Name <you@example.com>
2022-06-01 15:30:01 +0300

    commit 1

--- src/rosbank/Main.java
+++ src/rosbank/Main.java
@@ -2,11 +2,31 @@ package rosbank;
 
 public class Main {
 
-    public void task(){
+    public static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        int temp;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i+1]) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+
 
     }
 
     public static void main(String[] args) {
-        new Main().task();
+        int a[] = new int [482310];
+
+        new Main().bubbleSort(a);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 }
